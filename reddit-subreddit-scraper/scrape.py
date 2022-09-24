6baa88191fe18cd41f0916bce5c8e39d8d4f9a0c
AndrewYatzkan <andrew@yatzkan.com>
2022-09-23 21:15:32 -0500

    Fetch all posts in a given subreddit dating back to an arbitrary time, keeping track of top five posts every week for an arbitrary number of weeks

--- reddit-subreddit-scraper/scrape.py
+++ reddit-subreddit-scraper/scrape.py
@@ -1,4 +1,5 @@
 import click
+from heapq import heappush, heappushpop
 from utils import *
 
 @click.command()
@@ -10,7 +11,33 @@ from utils import *
 )
 def main(subreddit):
 	subreddit = format_subreddit(subreddit)
-	print(subreddit)
+
+	timestamp = time_ago(days=15)
+	posts = fetch_posts_since("uiuc", timestamp)
+
+	ONE_WK = 60 * 60 * 24 * 7
+	most_recent = posts[0]["created_utc"]
+
+	heaps = []
+	for x in range(7):
+		heaps.append([])
+	for post in posts:
+		score = post["score"]
+		created = post["created_utc"]
+		weeks_ago = int((most_recent - created) / ONE_WK)
+
+		if weeks_ago <= 6:
+			if len(heaps[weeks_ago]) < 5:
+				heappush(heaps[weeks_ago], (score, post["title"]))
+			else:
+				heappushpop(heaps[weeks_ago], (score, post["title"]))
+
+	# sorts the posts by week
+	for heap in heaps:
+		heap.sort(key=lambda x: x[0], reverse=True)
+
+	for post in heaps[3]:
+		print(post)
 
 if __name__ == "__main__":
 	main()
--- reddit-subreddit-scraper/utils.py
+++ reddit-subreddit-scraper/utils.py
@@ -1,5 +1,37 @@
+import requests
+import time
+
 def format_subreddit(subreddit):
-	prefix = "r/"
-	if subreddit[:2] == prefix:
-		return subreddit
-	return prefix + subreddit
+	if subreddit[:2] == "r/":
+		return subreddit[2:]
+	return subreddit
+
+def time_ago(seconds=0, minutes=0, hours=0, days=0, months=0, years=0):
+	return time.time() - (seconds + minutes * 60 + hours * 3600 + days * 86400 + months * 2592000 + years * 31536000)
+
+def fetch_posts(sub, after=None, limit=100):
+	r = requests.get(f"https://api.reddit.com/r/{sub}/new.json?limit={limit}&after={after}", headers={"User-Agent": "Subreddit Scraper v0.1"})
+	match r.status_code:
+		case 200: # success
+			res = r.json()["data"]
+			res["children"] = list(map(lambda x: x["data"], res["children"]))
+			return res
+		case 429: # rate-limited
+			time.sleep(2)
+			print("Sleeping 2s")
+			return fetch_posts(sub, after, limit)
+		case _: # unknown error
+			raise Exception(f"Unexpected status code {r.status_code}")
+
+def fetch_posts_since(sub, since):
+	res = fetch_posts(sub)
+	posts = res["children"]
+	after = res["after"]
+	delta = posts[-1]["created_utc"] - since
+	while delta > 0:
+		print(f"Fetched {len(posts)} post(s) -- Sifting through {delta // 86400} more day(s) worth of posts..")
+		res = fetch_posts(sub, after)
+		posts += res["children"]
+		after = res["after"]
+		delta = posts[-1]["created_utc"] - since
+	return posts
