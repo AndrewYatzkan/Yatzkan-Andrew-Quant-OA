b2d0bff6187fe59f3594373f1abdc19158e94299
AndrewYatzkan <andrew@yatzkan.com>
2022-09-23 23:10:40 -0500

    Pretty print file

--- reddit-subreddit-scraper/utils.py
+++ reddit-subreddit-scraper/utils.py
@@ -1,13 +1,34 @@
+import json
 import requests
 import time
-# from post import Post
+from heapq import heappush, heappushpop
 
 def format_subreddit(subreddit):
 	if subreddit[:2] == "r/":
 		return subreddit[2:]
 	return subreddit
 
-def format_post(post):
+def format_post(post_tuple):
+	post, comments = list(map(lambda x: x["data"], fetch_post_info(post_tuple[1])))
+	post = post["children"][0]["data"]
+	comments = comments["children"]
+
+	heap = []
+	N_TOP_COMMENTS = 10
+	for comment in comments:
+		comment = comment["data"]
+		try:
+			score = comment["score"]
+		except:
+			continue
+		author = comment["author"]
+		body = comment["body"]
+		if len(heap) < N_TOP_COMMENTS:
+			heappush(heap, (score, author, body))
+		else:
+			heappushpop(heap, (score, author, body))
+
+	post["top_replies"] = heap
 	return post
 
 def time_ago(seconds=0, minutes=0, hours=0, days=0, months=0, years=0):
@@ -24,7 +45,7 @@ def fetch_post_info(name):
 	r = requests.get(f"https://api.reddit.com/comments/{name}.json", headers={"User-Agent": "Subreddit Scraper v0.1"})
 	match r.status_code:
 		case 200: # success
-			return r.json()[1]["data"]["children"]
+			return r.json()
 		case 429: # rate-limited
 			time.sleep(2)
 			print("Sleeping 2s")
@@ -33,7 +54,7 @@ def fetch_post_info(name):
 			raise Exception(f"Unexpected status code {r.status_code}")
 
 def fetch_posts(sub, after=None, limit=100):
-	r = requests.get(f"https://api.reddit.com/r/{sub}/new.json?limit={limit}&after={after}", headers={"User-Agent": "Subreddit Scraper v0.1"})
+	r = requests.get(f"https://api.reddit.com/r/{sub}/new.json?limit={limit}&sort=new&after={after}", headers={"User-Agent": "Subreddit Scraper v0.1"})
 	match r.status_code:
 		case 200: # success
 			res = r.json()["data"]
@@ -56,5 +77,7 @@ def fetch_posts_since(sub, since):
 		res = fetch_posts(sub, after)
 		posts += res["children"]
 		after = res["after"]
+		if after is None:
+			break
 		delta = posts[-1]["created_utc"] - since
 	return posts
